6672921d4da053149fb0a6dde251d3b4e60ffc51
alexandrydebora <103550755+alexandrydebora@users.noreply.github.com>
2022-04-13 14:18:15 +0700

    Create TugasPrak2Debora.java

---
+++ tugasprak2debora/TugasPrak2Debora.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugasprak2debora;
+
+/**
+ *
+ * @author ACER
+ */
+public class TugasPrak2Debora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
